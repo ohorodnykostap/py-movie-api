8a8ca3cb599d915ae5b65fe06019fce0439b4cdb
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2025-12-28 22:53:19 +0200

    fix: correct movie API urls

--- cinema_service/cinema/urls.py
+++ cinema_service/cinema/urls.py
@@ -3,6 +3,6 @@ from cinema.views import movie_list, movie_detail
 
 
 urlpatterns = [
-    path('movies/', movie_list),
-    path('movies/<int:pk>/', movie_detail),
+    path("movies/", movie_list, name="movie-list"),
+    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
