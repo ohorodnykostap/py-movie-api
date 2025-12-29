558cb31dcaacbc08dd0219cf94b4336b1bf8dae9
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2025-12-29 17:45:58 +0200

    update gitignore file

--- cinema_service/cinema/views.py
+++ cinema_service/cinema/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
